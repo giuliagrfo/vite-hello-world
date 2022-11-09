fa9423bb9ac6e8776577e4202b03297a6f67d187
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-09 16:02:05 +0100

    bonus - add jumbotron

---
+++ vite-hello/src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <div class="jumbotron">
+            <img src="https://images.mubicdn.net/images/film/405/cache-47097-1612536788/image-w1280.jpg?size=800x"
+                alt="">
+            <div class="text">
+                <h3>All work and no play makes Jack a dull boy</h3>
+            </div>
+        </div>
+
+    </section>
+</template>
+
+<style>
+.text {
+    color: rgba(125, 125, 38, 0.963);
+    position: relative;
+    top: -80px;
+}
+</style>
